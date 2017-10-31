30e54e6d1c3e705d04a2c39e991c9885570595fc
David Muckle <dvdmuckle@dvdmuckle.xyz>
2017-10-31 15:46:18 +0000

    Add more notes

--- numberguess.lisp
+++ numberguess.lisp
@@ -15,3 +15,6 @@
   (defparameter *big* 100)
   (guess-my-number))
 ; Let can be used to define local variables, and flet can be used to define local functions. This would be useful for private functions, I suppose
+; Label can be used if you want to let a private function call itself in the body of the function
+; This is useful for private recursive functions
+; The 1- and 1+ functions (Yes those are functions) do the same as (- x 1) and ((+ x 1)
